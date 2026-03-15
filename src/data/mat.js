dccee4678c99feea43f4e54649cfce95fc71c2b5
delarum <delarum7@gmail.com>
2026-03-15 22:59:08 +0300

    Add matatu list data of area, name, route, type, operator, time spotted, direction, location, capacity and  fare

---
+++ src/data/mat.js
@@ -0,0 +1,100 @@
+const matatus = [
+  {
+    id: 1,
+    area: "Umoja Innercore",
+    matatuName: "Utimo",
+    routeNumber: "36/60",
+    vehicleType: "bus and elctric buses",
+    operator: "Utimo Sacco Limited",
+    timeSpotted: "10:50am",
+    location: "Utimo innercore",
+    capacity: "32 seater",
+    fareEstimate: "Ksh 70-100"
+  },
+  {
+    id: 2,
+    area: "Komarock",
+    matatuName: "Risen",
+    routeNumber: "19",
+    vehicleType: "Bus",
+    operator: "Risen Limited Sacco",
+    timeSpotted: "9:00am",
+    location: "Mtindwa from CBD",
+    capacity: "22 seater",
+    fareEstimate: "Ksh 50-100"
+  },
+  {
+    id: 3,
+    area: "Kikuyu,Kangemi",
+    matatuName: "Super Metro",
+    routeNumber: "105/115",
+    vehicleType: "Bus",
+    operator: "Metro Trans",
+    timeSpotted: "08:30 am",
+    location: "Uthiru towards Limuru",
+    capacity: "32 seater",
+    fareEstimate: "Ksh 80-100"
+  },
+  {
+    id: 4,
+    area: "Chiromo,Othaya",
+    matatuName: "Killeton",
+    routeNumber: "480",
+    vehicleType: "Nissan Matatu",
+    operator: "Killeton Sacco",
+    timeSpotted: "5:00pm",
+    location: "Kasuku center to wards CBD",
+    capacity: "14 passangers",
+    fareEstimate: "Ksh 50-60"
+  },
+  {
+    id: 5,
+    area: "Westlands",
+    matatuName: "Supermetro",
+    routeNumber: "23",
+    vehicleType: "Electric Bus",
+    operator: "Metro Trans",
+    timeSpotted: "08:30am",
+    location: "Parklands Police Station towards CBD",
+    capacity: "35",
+    fareEstimate: "Ksh 50-70"
+  },
+  {
+    id: 6,
+    area: "Eastleigh",
+    matatuName: "",
+    routeNumber: "6/28",
+    vehicleType: "Eastleigh Commuter",
+    operator: "Eastleigh Commuter Services",
+    timeSpotted: "7:00pm",
+    location: "Day to Day towards Marie Stopes",
+    capacity: "32",
+    fareEstimate: "Ksh40-50"
+  },
+  {
+    id: 7,
+    area: "Kitengela",
+    matatuName: "Super Metro",
+    routeNumber: "110AK",
+    vehicleType: "Bus",
+    operator: "Metro Trans",
+    timeSpotted: "3:00pm",
+    location: "Syokimau towards Kitengela",
+    capacity: "32 seater",
+    fareEstimate: "Ksh 100-120"
+  },
+  {
+    id: 8,
+    area: "Buruburu",
+    matatuName: "Buruburu 58 ",
+    routeNumber: "58",
+    vehicleType: "bus",
+    operator: "Buruburu 58 Sacco",
+    timeSpotted: "3:30 pm",
+    location: "Jogoo road towards Buru",
+    capacity: "32",
+    fareEstimate: "Ksh 80-100"
+  },
+  ];
+
+export default matatus;
